227f1a22fefd75a61bb8e7573a632992b9fbbfd9
Babatoonday Mattieu <32313099+bbmattieu9@users.noreply.github.com>
2021-01-25 21:40:46 +0100

    [App] refactor folder structure - add Header file

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+    return <div className='header'>
+            <h2>Here is the header!</h2>
+        </div>
+}
+
+export default Header
