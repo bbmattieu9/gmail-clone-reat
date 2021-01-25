3020d3adc0d375452a32f40e9be7dde07a347b07
Babatoonday Mattieu <32313099+bbmattieu9@users.noreply.github.com>
2021-01-25 23:56:47 +0100

    [Header] add header UI elements

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@
 import './App.css';
 import Header from './Header';
 
+
 function App() {
   return (
     <div className="App">
-      <h1>
-         Gmail Clone by Tizi Cerutti!
-      </h1>
       <Header />
     </div>
   );
--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,39 @@
+import { Avatar, IconButton } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import AppsIcon from '@material-ui/icons/Apps';
+import NotificationsIcon from '@material-ui/icons/Notifications'
 import React from 'react';
 import './Header.css';
 
 function Header() {
     return <div className='header'>
-            <h2>Here is the header!</h2>
+            
+            <div className="header__left">
+                <IconButton>
+                    <MenuIcon />
+                </IconButton>
+
+                <img src="https://cdn.vox-cdn.com/thumbor/ttJ6TTVOWTTi2p3EaeeLfS4fUS8=/0x0:1320x880/1400x933/filters:focal(555x335:765x545):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/67587450/newgmaillogo.0.jpg" alt=""/>
+            </div>
+
+            <div className="header__middle">
+                <SearchIcon />
+                <input type='text' placeholder='Search mail' />
+                <ArrowDropDownIcon className='header__inputCaret' />
+            </div>
+
+            <div className="header__right">
+                <IconButton>
+                    <AppsIcon />
+                </IconButton>
+
+                <IconButton>
+                    <NotificationsIcon />
+                </IconButton>
+                <Avatar />
+            </div>
         </div>
 }
 
