c6f12904c8644a7f912992b21ff5d35567e83fe9
Babatoonday Mattieu <32313099+bbmattieu9@users.noreply.github.com>
2021-01-29 23:21:18 +0100

    [EmailRow] refactor email Row

--- src/EmailRow.js
+++ src/EmailRow.js
@@ -6,35 +6,35 @@ import { Checkbox, IconButton } from '@material-ui/core';
 import StarBorderOutlinedIcon from '@material-ui/icons/StarBorderOutlined';
 import LabelImportantOutlinedIcon from '@material-ui/icons/LabelImportantOutlined';
 
-function EmailRow({ id, title, subject, description, time}) {
+function EmailRow({ id, title, subject, description, time }) {
     return <div className='emailRow'>
-            
-            <div className="emailRow__options">
-                <Checkbox />
-                <IconButton>
-                    <StarBorderOutlinedIcon />
-                </IconButton>
 
-                <IconButton>
-                    <LabelImportantOutlinedIcon />
-                </IconButton>
-            </div>
+        <div className="emailRow__options">
+            <Checkbox />
+            <IconButton>
+                <StarBorderOutlinedIcon />
+            </IconButton>
 
-            <div className="emailRow__title">
-                {title}
-            </div>
-
-            <div className="emailRow__message">
-                <h4>
-                    {subject} { ' ' }
-                    <span className="emailRow__description">{description}</span>
-                </h4>
-            </div>
-
-            <p className="emailRow__time">
-                {time}
-            </p>
+            <IconButton>
+                <LabelImportantOutlinedIcon />
+            </IconButton>
         </div>
+
+        <div className="emailRow__title">
+            {title}
+        </div>
+
+        <div className="emailRow__message">
+            <h4>
+                {subject} {' '}
+                <span className="emailRow__description">{description}</span>
+            </h4>
+        </div>
+
+        <p className="emailRow__time">
+            {time}
+        </p>
+    </div>
 }
 
 export default EmailRow
